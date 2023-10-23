8f5e3940a9188aac35b49a65b7e6e2a974b70bd8
Alena <alyonabainova2003@gmail.com>
2023-10-23 19:42:08 +0700

    first var

--- User.php
+++ User.php
@@ -2,7 +2,7 @@
 
 class User
 {
-    private ?\DateTimeImmutable $createdAt;
+    public ?DateTimeImmutable $createdAt;
 
     public function __construct(
         private readonly string $login,
@@ -14,7 +14,7 @@ class User
         $this->createdAt = $createdAt;
 
         if (null === $this->createdAt) {
-            $this->createdAt = new \DateTimeImmutable();
+            $this->createdAt = new DateTimeImmutable();
         }
     }
 
